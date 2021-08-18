513ecd760d5830ab6a1c286ff7e873610c44b852
Paul <paulbzzy@gmail.com>
2021-08-17 22:40:40 -0600

    Add build side effects

---
+++ build/precache-manifest.6cb5f0cd9c869bfcfe8ece0eb1128411.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05815be1daa6dccace0e5c0f576c80d5",
+    "url": "/kanbanist/index.html"
+  },
+  {
+    "revision": "120e95fd962e27636cd0",
+    "url": "/kanbanist/static/css/2.12c3dc4c.chunk.css"
+  },
+  {
+    "revision": "bedce3d2918129b670a4",
+    "url": "/kanbanist/static/css/main.39267c7e.chunk.css"
+  },
+  {
+    "revision": "120e95fd962e27636cd0",
+    "url": "/kanbanist/static/js/2.80cedc23.chunk.js"
+  },
+  {
+    "revision": "02b710033b8fd042fce3b8d1cb50719f",
+    "url": "/kanbanist/static/js/2.80cedc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bedce3d2918129b670a4",
+    "url": "/kanbanist/static/js/main.7f8976e6.chunk.js"
+  },
+  {
+    "revision": "ac85734e1bfdff0572c9",
+    "url": "/kanbanist/static/js/runtime-main.996f1300.js"
+  },
+  {
+    "revision": "03ab771b196ad90f28a35a5aa8f5ec08",
+    "url": "/kanbanist/static/media/icons-16.03ab771b.ttf"
+  },
+  {
+    "revision": "84c180eabdd692efd197345a33735e8c",
+    "url": "/kanbanist/static/media/icons-16.84c180ea.woff"
+  },
+  {
+    "revision": "9631870bf93f28bc1d7010bf55621ae8",
+    "url": "/kanbanist/static/media/icons-16.9631870b.eot"
+  },
+  {
+    "revision": "053082015844707d8048e606b922cdad",
+    "url": "/kanbanist/static/media/icons-20.05308201.ttf"
+  },
+  {
+    "revision": "5c61819764f1fa973dda8ef77e391451",
+    "url": "/kanbanist/static/media/icons-20.5c618197.woff"
+  },
+  {
+    "revision": "ca7934b30a0b9829dbeb0b58a6fd0b51",
+    "url": "/kanbanist/static/media/icons-20.ca7934b3.eot"
+  },
+  {
+    "revision": "94fc37cd00da58e41858d81d1de097cc",
+    "url": "/kanbanist/static/media/kanbanist.94fc37cd.png"
+  },
+  {
+    "revision": "5ea13674d5cd54db3631ee0002efd00b",
+    "url": "/kanbanist/static/media/pacifico-webfont.5ea13674.woff"
+  },
+  {
+    "revision": "67c0cf751e2bd72fb40bb2947804141e",
+    "url": "/kanbanist/static/media/pacifico-webfont.67c0cf75.woff2"
+  }
+]);
